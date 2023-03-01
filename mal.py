589be968192bc2b5f4b6890999132bbdacf4ec15
xzeqtr <pwrm@mail.ru>
2023-03-01 23:03:20 +0300

    Two pictures in one

--- mal.py
+++ mal.py
@@ -4,6 +4,7 @@ import sys
 import requests
 import pandas as pd
 from user_agent import generate_user_agent
+import matplotlib.pyplot as plt
 
 # generate a user agent
 headers = {'User-Agent': generate_user_agent(device_type="desktop", os=('mac', 'linux'))}
@@ -29,7 +30,7 @@ except:
     sys.exit()
 
 tables = page_content.find_all('table', {'class': 'list-table'})
-table_items = re.findall(r'data-items="(\[.*\])">', str(tables[0]) \
+table_items = re.findall(r'data-items="(\[.*])">', str(tables[0]) \
                          .replace('\\&quot;', "'") \
                          .replace('&quot;', '"'))
 
@@ -40,18 +41,54 @@ finaldf = df.drop(['status', 'created_at', 'updated_at', 'tags', 'anime_id', 'is
                    'anime_airing_status', 'has_episode_video', 'has_promotion_video', 'has_video',
                    'video_url', 'is_added_to_list', 'start_date_string', 'finish_date_string',
                    'anime_start_date_string', 'anime_end_date_string', 'days_string',
-                   'storage_string', 'priority_string', 'notes', 'editable_notes', 'title_localized',
-                   'anime_title_eng', 'anime_total_members', 'anime_total_scores',
-                   'demographics'], axis=1)
+                   'storage_string', 'priority_string', 'notes', 'editable_notes',
+                   'title_localized', 'anime_title_eng', 'anime_total_members',
+                   'anime_total_scores', 'demographics'], axis=1)
 finaldf.rename(columns={
-    "score":"User_score",
-    "anime_title":"Title",
-    "anime_num_episodes":"Episodes",
-    "anime_score_val":"Score",
-    "genres":"Genres",
-    "anime_url":"URL",
-    "anime_image_path":"Image",
-    "anime_media_type_string":"Type",
-    "anime_mpaa_rating_string":"Rating"}, inplace=True)
+    "score": "User_score",
+    "anime_title": "Title",
+    "anime_num_episodes": "Episodes",
+    "anime_score_val": "Score",
+    "genres": "Genres",
+    "anime_url": "URL",
+    "anime_image_path": "Image",
+    "anime_media_type_string": "Type",
+    "anime_mpaa_rating_string": "Rating"}, inplace=True)
 finaldf['Genres'] = finaldf['Genres'].apply(lambda x: ', '.join(map(lambda x: x['name'], x)))
 finaldf.to_csv('mal.csv', sep=';', encoding='utf-8')
+
+# ========================= Types =========================
+types = finaldf['Type'].value_counts().reset_index()
+types.columns = ['Type', 'Count']
+types_labels = [k+' - '+str(v) for k, v in zip(types['Type'], types['Count'])]
+types.set_index('Type', inplace=True)
+
+ax = types.plot.pie(
+    title='Anime type',
+    y='Count',
+    # shadow=True,
+    autopct='%.1f%%',
+    explode=[_/10 for _ in range(len(types['Count']))],
+    legend=False,
+    figsize=(9, 6),
+    labels=types_labels)
+ax.set_ylabel(None)
+plt.show()
+# ========================= Types =========================
+
+# ========================= Score =========================
+score = finaldf['User_score'].value_counts().reset_index()
+score.columns = ['User_score', 'Count']
+score.sort_values(by=['User_score'], ascending=False)
+
+score_labels = [str(k)+' - '+str(v) for k, v in zip(score['User_score'], score['Count'])]
+fig, axs = plt.subplots(1, 2, figsize=(9, 6))
+axs[0].pie(
+    score['Count'],
+    autopct='%.1f%%',
+    labels = score_labels)
+axs[1].bar(
+    score['User_score'],
+    score['Count'])
+plt.show()
+# ========================= Score =========================
